af8c199dd70e7a6914dcea1789b26080ab50bcb6
DanielNemchenko <danielnemchenko8@gmail.com>
2025-12-20 12:58:56 +0100

    Testing main (test_main.py) created to confirm saving and retrieving data to/from zookeeper

--- src/main.py
+++ src/main.py
@@ -5,11 +5,9 @@ import time
 import random
 import signal
 
-
 # Definir una función que se ejecuta cuando se recibe la señal de interrupción
 def interrupt_handler(signal, frame):
     exit(0)
-
 
 # Registrar la función como el manejador de la señal de interrupción
 # So the script exits cleanly when interrupted (Ctrl+C) :
@@ -19,21 +17,35 @@ signal.signal(signal.SIGINT, interrupt_handler)
 id = input("Introduce un identificador: ")
 
 # Crear un cliente kazoo y conectarlo con el servidor zookeeper
-client = KazooClient(hosts="127.0.0.1:2181")
-client.start()
+zk = KazooClient(hosts="127.0.0.1:2181")
+zk.start()
 
 # Crear una elección entre las aplicaciones y elegir un líder
-election = Election(client, "/election", id)
+election = Election(zk, "/election", id)
 
 
+# LEADER ONLY --------------------------------------------------------------
 # Definir una función que se ejecuta cuando una aplicación es elegida líder
 def leader_func():
     while True:
-        print('soy lider')
-        # Obtener los hijos de /mediciones
+        print("LEADER :")
+
+        # List the children
+        children = zk.get_children("/mediciones")
+        # Check :
+        print("There are %s children with names %s" % (len(children), children))
+
         # Calcular la media de los valores
+        if len(children) > 0:
+            mean_value = sum(children) / len(children)
+        else :
+            mean_value = 0
+
         # Mostrar la media por consola
+        print("The mean is %s" % mean_value)
+
         # Enviar la media usando requests
+
         time.sleep(5)
 
 
@@ -42,11 +54,27 @@ def election_func():
     # Participar en la elección con el identificador de la aplicación
     election.run(leader_func)
 
-
-# Crear un hilo para ejecutar la función election_func
+# Crear un hilo para ejecutar la función election_func:
+# daemon=True tells Python: "Do not keep the program alive just for this thread."
+# Standard Thread (daemon=False): 
+# If the main program finishes (e.g., reaches the end of the script), 
+# Python will wait for this thread to finish before actually closing the process.
 election_thread = threading.Thread(target=election_func, daemon=True)
+
 # Iniciar el hilo
 election_thread.start()
+
+
+
+# FOLLOWERS AND LEADER ------------------------------------------------------------------
+try:
+    # Ensure a path, create if necessary
+    zk.ensure_path("/mediciones")
+
+    # Create a node with data
+    zk.create(f"/mediciones/{id}", ephemeral=True)
+except:
+    print('ERROR: Node creation exception (maybe already exists)')
 
 # Enviar periódicamente un valor a una subruta de /mediciones con el identificador de la aplicación
 while True:
@@ -54,7 +82,10 @@ while True:
     # value = random.randint(75, 85)
     value = 80.0
 
-    # Actualizar el valor de /values asociado al nodo
+    # Modify the data of a node ...?? -> # Actualizar el valor de /values asociado al nodo
+    zk.set(f"/mediciones/{id}", value)
+    print(f"Node {id}: {value} sent to zk")
 
     # Esperar 5 segundos
     time.sleep(5)
+ 
--- src/test_main.py
+++ src/test_main.py
@@ -1,5 +1,5 @@
 import requests
-
+'''
 url = 'http://127.0.0.1:4001/nuevo?dato=99.2' # Definimos la URL a la que queremos hacer la petición
 
 print("Sending request...")
@@ -8,5 +8,36 @@ print("URL: " , response.url)
 print("status code : " ,response.status_code)
 print("text: " , response.text)
 print("response: " ,response)
+'''
+from kazoo.client import KazooClient
+
+zk = KazooClient(hosts="127.0.0.1:2181")
+zk.start()
+
+try:
+    # Ensure a path, create if necessary
+    zk.ensure_path("/mediciones")
+
+    # Create a node with data
+    zk.create(f"/mediciones/01", ephemeral=True)
+
+    zk.create(f"/mediciones/02", ephemeral=True)
+
+except:
+    print('ERROR: Node creation exception (maybe already exists)')
+
+print("Followers values set \n")
+zk.set(f"/mediciones/01", str(11).encode("utf-8"))
+zk.set(f"/mediciones/02", str(22).encode("utf-8"))
 
 
+print("LEADER :\n")
+
+# List the children
+children = zk.get_children("/mediciones")
+# Check :
+print("There are %s children with names %s" % (len(children), children))
+
+for child in children:
+    value, stats = zk.get(f"/mediciones/{child}")
+    print(f"Value of the child {child} : " , value.decode("utf-8"))
